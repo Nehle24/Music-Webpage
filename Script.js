f10fb0f589bf7bc6f180cebbab15c52514fe407c
Nehle24 <Nahlamrad1@gmail.com>
2025-11-13 20:06:55 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,15 @@
+﻿// Animate the pink box horizontally
+let box = document.getElementById("box");
+let pos = 0;
+let direction = 1;
+
+function animateBox()
+{
+    if (pos > 200 || pos < 0) direction *= -1; // Reverse direction
+    pos += direction * 2;
+    box.style.transform = `translateX(${ pos}
+    px)`;
+    requestAnimationFrame(animateBox);
+}
+
+animateBox();
