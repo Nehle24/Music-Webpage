c8a62b01746012f296f10489f8c16796f106b879
Nehle24 <Nahlamrad1@gmail.com>
2025-11-13 20:16:57 -0500

    Refactor box animation code for clarity

--- Script.js
+++ Script.js
@@ -1,14 +1,12 @@
-﻿// Animate the pink box horizontally
+// Animate the pink box horizontally
 let box = document.getElementById("box");
 let pos = 0;
 let direction = 1;
 
-function animateBox()
-{
+function animateBox() {
     if (pos > 200 || pos < 0) direction *= -1; // Reverse direction
     pos += direction * 2;
-    box.style.transform = `translateX(${ pos}
-    px)`;
+    box.style.transform = `translateX(${pos}px)`;
     requestAnimationFrame(animateBox);
 }
 
